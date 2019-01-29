4231e6b68555fdddc9aef9f6c5d82267cda2c2d6
Joanna Filipowska <j.filipowska@gmail.com>
2019-01-28 20:04:46 -0600

    adding checkpoint3

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import RecipeCard from "./components/RecipeCard";
+import "./App.css";
+import Navigation from "./components/Navigation";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      meal: {}
+    };
+  }
+  componentDidMount() {
+    const URL = "https://www.themealdb.com/api/json/v1/1/latest.php";
+    axios
+      .get(URL)
+      .then(res => {
+        const meal = res.data.meals;
+        if (typeof meal === "object") {
+          this.setState({ meal });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
   render() {
+    var data = this.state.meal;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navigation />
+        <AppContainer className="App">
+          {data.length > 0 && <RecipeCard meals={data} />}
+        </AppContainer>
       </div>
     );
   }
 }
+
+const AppContainer = styled.div`
+  background: #ddd;
+  padding: 2em;
+`;
 
 export default App;
